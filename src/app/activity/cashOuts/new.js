791f6ef6d6c8e0593b1132ecdfae248fa797644b
Cory Boyd <corytheboyd@gmail.com>
2014-04-12 16:23:52 -0700

    addressManager factory, cleans up some grossness regarding multiple address selects. addressSelect directive is no longer magical

--- src/app/activity/cashOuts/new.js
+++ src/app/activity/cashOuts/new.js
@@ -1,20 +1,56 @@
 'use strict';
 
 angular.module('activity').
-  controller('NewCashOutController', function($scope, $api, $window, $location, countries, usStates) {
+  controller('NewCashOutController', function($scope, $api, $window, $location, AddressManager, countries, usStates) {
 
     $scope.countries = countries;
     $scope.usStates = usStates;
 
     $scope.cashOut = {
-      type: undefined,
+      type: 'check', // undefined,
       amount: undefined,
-      address: undefined,
       paypal_address: undefined,
       bitcoin_address: undefined,
-      mailing_address: undefined,
+      address_id: undefined,
+      mailing_address_id: undefined,
       us_citizen: undefined
     };
+
+    // Create intance of AddressManager. Created this so that mailing_address and address
+    // could easily share the same array of addresses.
+    $scope.addressManager = new AddressManager($api.v2.addresses().then(function(response) {
+      var addresses = angular.copy(response.data);
+
+      // Initialize with the first address
+      if (angular.isArray(addresses) && addresses.length > 0) {
+        $scope.cashOut.address_id = addresses[0].id;
+        $scope.cashOut.mailing_address_id = addresses[0].id;
+      }
+
+      return addresses;
+    }));
+
+    // Initialize value of address selects if undefined
+    $scope.$on($scope.addressManager.addressAddedEvent, function(event, address) {
+      if (angular.isUndefined($scope.cashOut.address_id)) {
+        $scope.cashOut.address_id = address.id;
+      }
+
+      if (angular.isUndefined($scope.cashOut.address_id)) {
+        $scope.cashOut.mailing_address_id = address.id;
+      }
+    });
+
+    // Remove address values from selects where the removed address is the value.
+    $scope.$on($scope.addressManager.addressRemovedEvent, function(event, address) {
+      if (angular.isDefined($scope.cashOut.address) && address.id === $scope.cashOut.address.id) {
+        $scope.cashOut.address_id = undefined;
+      }
+
+      if (angular.isDefined($scope.cashOut.mailing_address) && address.id === $scope.cashOut.mailing_address.id) {
+        $scope.cashOut.mailing_address_id = undefined;
+      }
+    });
 
     // Set default to amount to current account balance
     $scope.$watch('current_person', function(person) {
@@ -23,54 +59,8 @@ angular.module('activity').
       }
     });
 
-    $scope.showNewAddressForm = function() {
-      $scope._showNewAddressForm = true;
-    };
-
-    $scope.hideNewAddressForm = function() {
-      $scope._showNewAddressForm = false;
-    };
-
-    $scope.createNewAddress = function() {
-      var payload = angular.copy($scope.newAddress);
-
-      $api.v2.createAddress(payload).then(function(response) {
-        // Push addresses into array
-        $scope.addresses.push(angular.copy(response.data));
-
-        // Set selected address to the newly created one
-        $scope.cashOut.address = $scope.addresses[$scope.addresses.length - 1];
-
-        $scope.hideNewAddressForm();
-      });
-    };
-
-    $scope.destroySelectedAddress = function() {
-      if ($window.confirm("Are you sure?")) {
-        $api.v2.destroyAddress($scope.cashOut.address.id).then(function() {
-          for (var i=0; i<$scope.addresses.length; i++) {
-            if ($scope.addresses[i].id === $scope.cashOut.address.id) {
-              $scope.addresses.splice(i,1);
-              $scope.cashOut.address = undefined;
-              break;
-            }
-          }
-        });
-      }
-    };
-
     $scope.createCashOut = function() {
-      var payload = {
-        type: $scope.cashOut.type,
-        amount: $scope.cashOut.amount,
-        address_id: $scope.cashOut.address && $scope.cashOut.address.id,
-        mailing_address_id: $scope.cashOut.mailing_address && $scope.cashOut.mailing_address.id,
-        paypal_address: $scope.cashOut.paypal_address,
-        bitcoin_address: $scope.cashOut.bitcoin_address,
-        us_citizen: $scope.cashOut.us_citizen
-      };
-
-      $api.v2.createCashOut(payload).then(function(response) {
+      $api.v2.createCashOut($scope.cashOut).then(function(response) {
         if (response.success) {
           $location.url('/activity/cash_outs');
         } else {
